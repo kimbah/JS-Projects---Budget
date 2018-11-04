4d9ea025b537d0aa87b412279b21e10145cd6353
kimbah <kim8hughes@iinet.net.au>
2018-11-04 11:57:21 +0800

    get and display budget added

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -107,7 +107,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -157,7 +161,19 @@ var UIController = (function () {
 
         getDOMstrings: function () {
             return DOMstrings;
-        }
+        },
+
+        displayBudget: function (obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '----';
+            }
+        },
     };
 
 })();
@@ -186,7 +202,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetController.getBudget();
 
         // 63 Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
 
@@ -215,8 +231,14 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     return {
         init: function () {
-            setupEventListerners();
             console.log('Application started');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
+            setupEventListerners();
         }
     };
 
